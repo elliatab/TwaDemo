86d3b677cdcaa7a0ca056c3c4526ae53f3629b09
Bataille, Julien | Julien | SSED <julien.bataille@rakuten.com>
2019-02-19 11:38:41 +0100

    Added intent filter and code cleanup

--- app/src/main/java/com/firebaseapp/twademo/MainActivity.java
+++ app/src/main/java/com/firebaseapp/twademo/MainActivity.java
@@ -3,6 +3,7 @@ package com.firebaseapp.twademo;
 import android.content.ComponentName;
 import android.net.Uri;
 import android.os.Bundle;
+
 import android.support.annotation.Nullable;
 import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
@@ -11,10 +12,9 @@ import android.support.customtabs.CustomTabsSession;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import android.support.customtabs.TrustedWebUtils;
 
@@ -27,16 +27,8 @@ public class MainActivity extends AppCompatActivity {
   @Nullable
   private TwaCustomTabsServiceConnection mServiceConnection;
 
-  private String mChromePackage;
-
-  /** We only want to show the update prompt once per instance of this application. */
-  private static boolean sChromeVersionChecked;
-
   @Nullable
   CustomTabsIntent mCustomTabsIntent;
-
-  @Nullable
-  private CustomTabsSession mCustomTabsSession;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-
         if (mCustomTabsIntent != null) {
           TrustedWebUtils.launchAsTrustedWebActivity(
               MainActivity.this,
@@ -59,25 +50,14 @@ public class MainActivity extends AppCompatActivity {
       }
     });
 
-
-    mChromePackage = CustomTabsClient.getPackageName(this,
+    String customTabsProviderPackage = CustomTabsClient.getPackageName(this,
         TrustedWebUtils.SUPPORTED_CHROME_PACKAGES, false);
 
-    if (mChromePackage == null) {
-      TrustedWebUtils.showNoPackageToast(this);
-      finish();
-      return;
-    }
-
-    if (!sChromeVersionChecked) {
-      TrustedWebUtils.promptForChromeUpdateIfNeeded(this, mChromePackage);
-      sChromeVersionChecked = true;
-    }
-
+    // TODO: in a production app we would check if a chrome package is available and if it's updated.
 
     mServiceConnection = new TwaCustomTabsServiceConnection();
-    CustomTabsClient.bindCustomTabsService(this, mChromePackage, mServiceConnection);
-
+    CustomTabsClient.bindCustomTabsService(
+        this, customTabsProviderPackage, mServiceConnection);
   }
 
   @Override
@@ -88,41 +68,6 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
-
-  protected CustomTabsSession getSession(CustomTabsClient client) {
-    return client.newSession(null, SESSION_ID);
-  }
-
-  /**
-   * Creates a {@link CustomTabsIntent} to launch the Trusted Web Activity.
-   * By default, Trusted Web Activity will be launched in the same Android Task.
-   * Override this if you want any special launching behaviour.
-   */
-  protected CustomTabsIntent getCustomTabsIntent(CustomTabsSession session) {
-    return new CustomTabsIntent.Builder(session).build();
-  }
 
   private class TwaCustomTabsServiceConnection extends CustomTabsServiceConnection {
 
@@ -130,17 +75,16 @@ public class MainActivity extends AppCompatActivity {
     public void onCustomTabsServiceConnected(ComponentName componentName,
                                              CustomTabsClient client) {
 
-      if (TrustedWebUtils.warmupIsRequired(MainActivity.this, mChromePackage)) {
-        client.warmup(0);
-      }
+      // Creates a CustomTabsSession with a constant session id.
+      CustomTabsSession session  = client.newSession(null, SESSION_ID);
 
-      mCustomTabsSession = getSession(client);
-      mCustomTabsIntent  = getCustomTabsIntent(mCustomTabsSession);
+      // Creates a CustomTabsIntent to launch the Trusted Web Activity.
+      mCustomTabsIntent  = new CustomTabsIntent.Builder(session).build();
     }
 
     @Override
     public void onServiceDisconnected(ComponentName componentName) {
-      Log.d(TAG, "TwaCustomTabs Service Disconnected");
+      Log.d(TAG, "Twa CustomTab Service Disconnected");
     }
   }
 }
